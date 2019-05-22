a93fd16a6aeb55531f6047fa04a22cd60c56bf4b
Laggage <1634205628@qq.com>
2019-05-22 16:24:33 +0800

    Clean project

--- UITest/ViewModels/AppViewModel.cs
+++ UITest/ViewModels/AppViewModel.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using Microsoft.Practices.Prism.ViewModel;
-using UITest.Models;
+﻿using Microsoft.Practices.Prism.ViewModel;
 
 namespace UITest.ViewModels
 {
@@ -8,43 +6,6 @@ namespace UITest.ViewModels
     {
         public AppViewModel()
         {
-            _tip = "SelfDefineTest";
-            if(Images == null)
-                Images = new List<Image>()
-                {
-                    new Image(@"E:\Media\Images\壁纸\ (1).jpg"),
-                    new Image(@"E:\Media\Images\壁纸\ (40).jpg"),
-                    new Image(@"E:\Media\Images\壁纸\ (41).jpg")
-                };
-        }
-
-
-        private List<Image> _images;
-
-        public List<Image> Images
-        {
-            get=> _images;
-            set
-            {
-                if (Equals(value, _images))
-                    return;
-                RaisePropertyChanged(nameof(Images));
-                _images = value;
-            }
-        }
-
-        private string _tip;
-
-        public string Tip
-        {
-            get => _tip;
-            set
-            {
-                if (_tip == value)
-                    return;
-                RaisePropertyChanged(nameof(Tip));
-                _tip = value;
-            }
         }
     }
 }
